155c17db8dc82685683ab9e9de1a6d01a4a0391f
behro <behro@LAPTOP-L5Q9J4LT>
2024-04-15 12:54:42 -0400

    Add project files.

---
+++ FinalBehroseFaisal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalBehroseFaisal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FinalBehroseFaisal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalBehroseFaisal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
